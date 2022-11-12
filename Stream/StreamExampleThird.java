f8ca3ba29e29da0bd731ea9737c364bf8143e60e
min1512 <min1512@hanmail.net>
2022-11-12 22:33:53 +0900

    StreamExample.reduce() 추가

--- Stream/StreamExampleThird.java
+++ Stream/StreamExampleThird.java
@@ -1,15 +1,16 @@
 package Stream;
 
-import java.util.Comparator;
-import java.util.stream.IntStream;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class StreamExampleThird {
     public static void main(String[] args) {
-        Stream<Integer> stream = IntStream.range(1,10).boxed();
-        stream.filter(v -> ( v%2 == 0 ) )
-                .map( v -> v*10 )
-                .sorted(Comparator.reverseOrder())//역정렬
-                .reduce( (x,y) -> x+y );
+//        Stream<Integer> numbers = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//        Optional<Integer> sum = numbers.reduce((x, y) -> x + y);
+//        sum.ifPresent(s -> System.out.println("sum: " + s));
+
+        Stream<Integer> numbers = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Integer sum = numbers.reduce(10, (total, n) -> total + n);
+        System.out.println("sum: " + sum);
     }
 }
